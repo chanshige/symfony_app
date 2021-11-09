feb83336629caac6b1dc0a880ba0c2581368ecde
Shigeki Tanaka <dev@shigeki.tokyo>
2021-11-09 21:17:49 +0900

    experimental validation

--- www/src/Controller/GreetingController.php
+++ www/src/Controller/GreetingController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Domain\Greeting\Input;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class GreetingController
 {
     #[Route(path: '/greeting')]
-    public function index(LoggerInterface $logger)
+    public function __invoke(Request $request, ValidatorInterface $validator, LoggerInterface $logger)
     {
         $logger->info('autowired !!!!!!!!');
 
-        return new JsonResponse(['greeting' => sprintf('Hi! %s Hello.', 'test')]);
+        $input = new Input($request->get('username', 'sample_test'));
+
+        $errors = $validator->validate($input);
+        if ($errors->count()) {
+            throw new HttpException(404, (string) $errors);
+        }
+
+        return new JsonResponse(['greeting' => sprintf('Hi! %s Hello.', $input->username())]);
     }
 }
